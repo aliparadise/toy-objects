3833d23030b0cdddbbbebefcebfbe874fb2fff16
Ali Paradise <alison.paradise@gmail.com>
2021-11-16 13:29:21 -0600

    JavaScript array and object practice

---
+++ leonids-toys.js
@@ -0,0 +1,37 @@
+const toys= [
+{
+    name: "Barbie doll",
+    id: 1,
+    type: "doll",
+    catagory: "pretend play",
+    manufacturerName: "Mattel",
+    inStock: 50,
+    ordered: 10,
+    needToOrder: false,
+    price: 9.99,
+},
+{
+    name: "wooden train",
+    id: 2,
+    type: "caboose",
+    catagory: "model building",
+    manufacturerName: "Hape",
+    inStock: 5,
+    ordered: 2,
+    needToOrder: true,
+    price: 5.99 
+},
+{
+    name: "Lego city",
+    id: 3,
+    type: "clock tower",
+    catagory: "construction",
+    manufacturerName: "Lego",
+    inStock: 1,
+    ordered: 0,
+    needToOrder: true,
+    price: 19.99,
+}
+]
+
+console.log(toys)
